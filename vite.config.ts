0547705a3fad7583421d9609a10600070f70d335
Vadim Sokhatsky <vs.frlnc@gmail.com>
2026-07-07 15:49:48 +0300

    feat: configure branch deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vsokhatskyi-sandbox-1/',
+  base: process.env.VITE_BASE ?? '/vsokhatskyi-sandbox-1/',
   plugins: [react()],
 })
