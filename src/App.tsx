3b8a28cd3fc472b3c8cb3c5d334932f3ad8ceb32
Vadim Sokhatsky <vs.frlnc@gmail.com>
2026-07-07 15:38:48 +0300

    feat: add layout and styles for squares

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
   return (
     <main className="app">
-      <h1 className="project-name">vsokhatskyi-sandbox-1</h1>
+      <div className="squares">
+      </div>
     </main>
   )
 }
